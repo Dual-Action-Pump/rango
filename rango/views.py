150fed463ce10c5d09030e885da76c2904ff8695
Mauricio Morales <mmorale8@binghamton.edu>
2017-01-14 16:07:59 -0500

    created views and linked to URL's

---
+++ rango/views.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+
+from django.http import HttpResponse
+
+# Create your views here.
+
+def index(request):
+	return HttpResponse("Rango says, Hey there partner!")	
+
+def test(request):
+	return HttpResponse("This is a test.")
